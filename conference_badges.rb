8e4cbbeddd88eda866eff8394e5875467583b1cc
paige <paigetetz@gmail.com>
2022-11-23 11:57:04 -0500

    commit tree c0513bbb686bbd3a803d3da89a5c5e74505ac662

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,30 @@
-# Write your code here.
+
+
+def badge_maker name
+    "Hello, my name is #{name}."
+end
+
+test = badge_maker("Ariel")
+puts test
+def batch_badge_creator(attendees)
+    attendees.map do |name|
+        "Hello, my name is #{name}."
+    end
+end
+
+def assign_rooms (attendees)
+    @room = 0
+    attendees.map do |attendee|
+        @room += 1
+        "Hello, #{attendee}! You'll be assigned to room #{@room}!"
+    end
+end
+def printer (attendees)
+    attendees.each do |attendee|
+        puts batch_badge_creator attendee
+    end
+    attendees.each do |attendee|
+    puts assign_rooms attendee
+    end
+end 
+printer (["Arel", "Carol"])
